6c79f87f35677cdd99093e375cd6e14292efb6da
ahmedshafin <167693675+ahmedshafin@users.noreply.github.com>
2024-05-24 13:03:27 +0600

    Add files via upload
    Avaiable Team/Assign Team/Resolve button

--- app1/views.py
+++ app1/views.py
@@ -11,125 +11,6 @@ from .decorators import unauthenticated_user, admin_only
 
 # Create your views here.
 
-def homeView(request) :
-    return render(request, "home.html")
-
-
-
-def viewHomepage(request) :
-               
-    return render(request, "index.html") 
-
-
-
-#User signUp
-def signUpView(request):
-    #Create user code
-    if  request.method =='POST': 
-        userName = request.POST.get('username')
-        email = request.POST.get('email')
-        password = request.POST.get('pass1')
-        confirmPassword = request.POST.get('pass2')
-        if password != confirmPassword:
-            return HttpResponse("Your password and confirm password doesn't match")
-        else:    
-            myUser = User.objects.create_user(userName, email, password)
-            myUser.save()
-            return redirect('clickPicture')
-        #User have been created
-    return render(request, "signup.html")
-
-
-
-
-
-
-
-#User Authentication
-def loginView(request) :
-    if  request.method == 'POST' :
-        loginName = request.POST.get('logName')
-        loginPass = request.POST.get('logPass')
-        
-        user = authenticate(request,username = loginName, password = loginPass )
-        if loginName == "admin":
-            if user is not None :
-                login(request, user)
-                return redirect('dashboardView')
-            else:
-                return HttpResponse("Username or Password is incorrect")  
-        else:   
-            if user is not None :
-                login(request, user)
-                return redirect('faceRecog')
-            else:
-                return HttpResponse("Username or Password is incorrect") 
-        
-    return render(request, "index.html")     
-
-  
-
-
-
-
-
-#User Report
-def reportView(request) :
-    if request.method =='POST' :
-        locate = request.POST.get('location')
-        reason = request.POST.get('cause')
-        extentDamage = request.POST.get('damage')
-        description = request.POST.get('comments')
-        latitude = request.POST.get('latitude')
-        longitude = request.POST.get('longitude')
-        
-        # Do something with latitude and longitude  
-        rep = report(location=locate, cause=reason, damage=extentDamage, comments=description, latitude=latitude,
-          longitude=longitude)
-        rep.save()
-        return redirect('home')
-    
-    return render(request, "report.html")
-
-
-
-def aboutView(request) :
-    return render(request, "about.html")
-
-
-
-
-
-
-#Adding Certification to User
-def certificateView(request):
-    restaurants = addRestaurentModel.objects.filter()
-    args = {
-        "restaurants": restaurants,  
-    }
-    return render(request, "certificate.html", args)
-
-
-
-#Adding Certification from Admin 
-def addRestaurentView(request):
-    if request.method =='POST' :
-        companyName = request.POST.get('company')
-        issue = request.POST.get('issuedBy')
-        expiry = request.POST.get('expiryDate')
-        message = request.POST.get('details')
-
-        #Exception handling
-        if not companyName or not issue or not expiry or not message:
-            return render(request, 'addRestaurent.html', {'error': 'All fields are required'})
-
-        add = addRestaurentModel(commpanyName=companyName, issuedBy=issue, expiryDate=expiry, message=message)
-        add.save()
-        return redirect('dashboardView')
-    return render(request, 'addRestaurent.html')
-
-
-
 
 
 
@@ -147,21 +28,39 @@ def map(request, slug):
     google_maps_url += "&q=nearby river"
     return redirect(google_maps_url)
     
+def deleteContact(request, slug):
+    certificate = contactUsModel.objects.get(id=slug)
+    certificate.delete()
+
+        # Appending the search query for nearby lakes to the Google Maps URL
+    
+    return redirect('displayContact')
 
 
+   
 
 
+#Available Teams
+def addTeam(request):
+    if request.method == 'POST':
+        teamName = request.POST.get('name')  # Get the team name from the form
+        # Create a new Team instance and save it to the database
+        newTeam = team(name=teamName)
+        newTeam.save()
+        return redirect('addTeam')
+    
+    return render(request, 'availableteam.html', {'teams': team.objects.all()}) 
 
 
-#Delete Certificate
-def deleteCertificate(request, slug):
-    delCertificate = addRestaurentModel.objects.get(id=slug)
-    delCertificate.delete()
+#Delete Team
+def deleteTeam(request, slug):
+    delTeam = team.objects.get(id=slug)
+    delTeam.delete()
 
-    return redirect('dashboardView')
+    return redirect('addTeam')
 
 
-
+    
 
 #Resolving and Backlog
 def resolve(request, slug):

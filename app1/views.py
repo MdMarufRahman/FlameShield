b413071f83d64c8c15f4b4a539bacae90f7943d2
MdMarufRahman <165766519+MdMarufRahman@users.noreply.github.com>
2024-05-24 15:08:52 +0600

    Update views.py

--- app1/views.py
+++ app1/views.py
@@ -11,6 +11,49 @@ from .decorators import unauthenticated_user, admin_only
 
 # Create your views here.
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def homeView(request) :
     return render(request, "home.html")
 
